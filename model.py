3e39d652b3df31277bb7bc410e5c1b79f5b94cf4
REXWindW <1114864532@qq.com>
2024-03-24 14:49:07 +0800

    update llama MLP，BLOCK

--- model.py
+++ model.py
@@ -1,6 +1,18 @@
 import torch
 import torch.nn as nn
 
+@dataclass
+# 模型参数
+class model_args:
+    block_size: int = 1024
+    # vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    # 我这里用
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    dropout: float = 0.0 # 默认不dropout
+    bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+
 class RMS_Norm(nn.Module):
     # 参考llama使用RMS Norm
     def __init__(self,hidden_size,eps=1e-6):
@@ -16,8 +28,8 @@ class RMS_Norm(nn.Module):
         # 这里计算L2范式/n后开根，详见RMS Norm的定义
         return self.weight * hidden_states/(sqrt_pow_mean+self.eps)
 
-class Attention(nn.Module):
-    # 注意力
+class flash_att(nn.Module):
+    # 参考NanoGPT
     def __init__(self,args):
         super().__init__()
         # qkv合到一个Linear里面去
@@ -31,6 +43,7 @@ class Attention(nn.Module):
         # dropout
         self.dropout = args.dropout # 这里是存布尔值，参数dropout概率，generate时设置为0即可
         self.att_dropout = nn.Dropout(self.dropout)
+        # 等价于nn.Dropout(p=self.dropout)
         # projection layer
         self.proj = nn.Linear(self.n_embed,self.n_embed, bias = args.bias)
 
@@ -56,5 +69,53 @@ class Attention(nn.Module):
         y = y.contiguous().view(B,T,C) # (B,T,C)
         
         # 输出时经过投影层后dropout
-        y = self.att_dropout()
+        return self.att_dropout(self.proj(y))
+        
+
+class MLP(nn.Module):
+    # MLP部分参考llama MLP结构
+    def __init__(self,args):
+        super.__init__()
+        self.dropout = nn.Dropout(args.dropout)
+        self.up_proj = nn.Linear(args.n_embed, 4*args.n_embed, bias = args.bias)
+        self.down_proj = nn.Linear(4*args.n_embed, args.n_embed, bias = args.bias)
+        # 使用relu
+        self.act_func = nn.functional.relu
+        # 学习llama增加一个门控
+        self.gate = nn.Linear(args.n_embed, 4*args.n_embed, bias = args.bias)
+
+    def forward(self, x):
+        # llama代码把MLP输入X切片成slice，我这里就不切片了
+        gate_proj = self.gate(x)
+        x = self.up_proj(x)
+
+        # llama中的代码：
+        # intermediate_states = (self.act_fn(gate_proj) * up_proj).split(slice, dim=2)
+        # nanogpt的
+        # x = self.act_func(x)
+        # 发现这里区别主要在，nanogpt对upproj的x使用激活函数，llama则是对gate使用
+
+        x = self.act_func(gate_proj)*x # 和门控gate按照对应位置相乘
+        x = self.down_proj(x)
+        return self.dropout(x)
     
+class Block(nn.Module):
+    # 之后用来堆叠的block
+    def __init__(self, args):
+        super().__init__()
+        self.norm = RMS_Norm(args.n_embed)
+        self.attn = flash_att(args)
+        self.mlp = MLP(args)
+
+    def forward(self,x):
+        # 使用pre norm
+        x = x + self.attn(self.norm(x))# residual
+        return x + self.mlp(self.norm(x)) # 残差链接
+
+class GPT(nn.Module):
+    # llama和GPT2的缝合怪
+    def __init__(self,args):
+        super().__init__()
+        
+        # 我打算是直接使用transformers库的tokenizer和embedding
+        
